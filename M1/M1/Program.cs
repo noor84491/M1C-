cb56e8ec8631d12169cfb25c8371d27ffe264f67
noor84491 <118957132+noor84491@users.noreply.github.com>
2024-11-24 17:38:34 +0400

    M1 C# YAY!!

---
+++ M1/M1/Program.cs
@@ -0,0 +1,31 @@
+﻿class MC
+{
+    static void Main(string[] args)
+    {
+
+        Console.WriteLine("Welcome to the rip-off of hangman");
+        Console.WriteLine("Guess the word by entering 1 latter ata time");
+
+
+        int attemp = 6;
+        bool guessed = false;
+        string wordtoguess = "apple";
+        char[] letters = new string('_', wordtoguess.Length).ToCharArray();
+
+
+        while (attemp > 0 && !guessed)
+        {
+
+
+            Console.WriteLine(wordtoguess);
+            Console.WriteLine('Guessed Wrong: ' + attemp);
+            Console.WriteLine('Enter a Letter: ');
+            char input = char.ToLower(Console.ReadKey().KeyChar);
+
+
+
+        }
+
+    }
+}
+
